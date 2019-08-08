6bb447aaf72128cd22895edc260a307a4a7062f6
robtf9 <robert.b.s@live.co.uk>
2019-08-08 13:53:08 +0100

    Filtered using regex in node file.

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,7 +7,9 @@ exports.createPages = ({ graphql, actions }) => {
   return new Promise((resolve, reject) => {
     graphql(`
       {
-        allMarkdownRemark {
+        allMarkdownRemark(
+          filter: { fileAbsolutePath: { regex: "/projects/" } }
+        ) {
           edges {
             node {
               frontmatter {
