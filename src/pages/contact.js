ea50ea28df7fba8b849e158a59cc61e3795c7594
robtf9 <robert.b.s@live.co.uk>
2019-08-09 16:06:12 +0100

    Reverting to custom behaviour.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,43 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import SEO from "../components/Layout/SEO";
 import Container from "../components/Common/Container";
 import Hero from "../components/Common/Hero";
 
 const Contact = () => {
-  const encode = data => {
-    return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-  };
-
-  const [enquiry, setEnquiry] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [sent, submit] = useState(false);
-
-  function handleChange(event) {
-    setEnquiry({
-      ...enquiry,
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  function handleSubmit(event) {
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", ...enquiry }),
-    })
-      .then(() => alert("Success!"))
-      .catch(error => alert(error));
-
-    event.preventDefault();
-  }
-
-  const { name, email, message } = enquiry;
-
+  // const submitHandler = event => {
+  //   event.preventDefault();
+  //   console.log("Submitted");
+  // };
   return (
     <>
       <SEO title="Contact" />
@@ -45,42 +15,28 @@ const Contact = () => {
         Get in <span>contact.</span>
       </Hero>
       <Container>
-        <form onSubmit={handleSubmit}>
-          <p>
-            <label>
-              Your Name:
-              <input
-                type="text"
-                name="name"
-                value={name}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email:
-              <input
-                type="email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:
-              <textarea
-                name="message"
-                value={message}
-                onChange={handleChange}
-              />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
+        <form
+          name="Contact Form"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          action="/"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="Contact Form" />
+          <div>
+            <label>Your name</label>
+            <input type="text" name="name" />
+          </div>
+          <div>
+            <label>Your email</label>
+            <input type="email" name="email" />
+          </div>
+          <div>
+            <label>Message</label>
+            <textarea name="message" />
+          </div>
+          <button type="submit">Send</button>
         </form>
       </Container>
     </>
