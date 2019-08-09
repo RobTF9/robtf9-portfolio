a359ba69312d2b90b516c2ad17111f09fa4c3521
robtf9 <robert.b.s@live.co.uk>
2019-08-09 14:57:10 +0100

    Form test.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,41 @@
 import React from "react";
 import SEO from "../components/Layout/SEO";
+import Container from "../components/Common/Container";
+import Hero from "../components/Common/Hero";
 
 const Contact = () => {
+  const submitHandler = event => {
+    event.preventDefault();
+    console.log("Submitted");
+  };
   return (
     <>
       <SEO title="Contact" />
-      <div>
-        <h1>Contact</h1>
-        <p>Hello I'm Rob</p>
-      </div>
+      <Hero>
+        Get in <span>contact.</span>
+      </Hero>
+      <Container>
+        <form
+          name="Contact Form"
+          method="POST"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+          action="#"
+          onSubmit={submitHandler}
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="Contact Form" />
+          <div>
+            <label>Your Email:</label>
+            <input type="email" name="email" />
+          </div>
+          <div>
+            <label>Message:</label>
+            <textarea name="message" />
+          </div>
+          <button type="submit">Send</button>
+        </form>
+      </Container>
     </>
   );
 };
