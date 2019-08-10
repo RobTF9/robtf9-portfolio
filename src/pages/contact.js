f3820587dd89a7d4ebddef7bcc481522078f6464
robtf9 <robert.b.s@live.co.uk>
2019-08-10 16:44:49 +0100

    Contact styling.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -34,22 +34,23 @@ const Contact = () => {
             <label>
               <h4>Your name</h4>
             </label>
-            <input type="text" name="name" />
+            <input type="text" name="name" required />
           </Input>
           <Input>
             <label>
               <h4>Your email</h4>
             </label>
-            <input type="email" name="email" />
+            <input type="email" name="email" required />
           </Input>
           <Input>
             <label>
               <h4>Message</h4>
             </label>
-            <textarea name="message" />
+            <textarea name="message" required />
           </Input>
           <Button type="submit">
             <h4>Send</h4>
+            <ButtonSpan />
           </Button>
         </form>
       </ContactContainer>
@@ -86,7 +87,29 @@ const Input = styled.div`
 `;
 
 const Button = styled.button`
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
   background-color: ${colors.blue};
   color: ${colors.white};
-  padding: 1.5rem 5rem;
+  padding: 2.5rem 6rem;
+  width: 100%;
+`;
+
+const ButtonSpan = styled.span`
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${colors.black};
+  opacity: 0.5;
+  transform: scale3d(1, 0, 1);
+  transform-origin: 50% 100%;
+  transition: transform 0.15s ease-in-out;
+
+  ${Button}:hover & {
+    transform: scale3d(1, 1, 1);
+  }
 `;
