598a791512153f62d1347bd57df52984d8000790
Robert Squires <robert.squires@proctors.co.uk>
2019-08-22 16:30:39 +0100

    Chore

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SEO from "../components/Layout/SEO";
 import HomeHero from "../components/Home/HomeHero";
 import ProjectList from "../components/Common/ProjectList";
+import { useSpring, animated, config } from "react-spring";
 
-const IndexPage = () => (
-  <>
-    <SEO title="Home" />
-    <HomeHero />
-    <ProjectList />
-  </>
-);
+const IndexPage = () => {
+  return (
+    <>
+      <SEO title="Home" />
+      <HomeHero />
+      <ProjectList />
+    </>
+  );
+};
 
 export default IndexPage;
