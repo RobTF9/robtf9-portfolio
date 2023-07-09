cf2bd8764a5deb1f6e79e686a5976ea921d0231d
Rob Squires <rsquires@magnetic-rock.com>
2023-07-09 10:41:03 +0100

    styled components fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,48 @@
-import StyledComponentsRegistry from '@/lib/registry'
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <StyledComponentsRegistry>
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
         <body>
           <Main />
           <NextScript />
         </body>
-      </StyledComponentsRegistry>
-    </Html>
-  )
+      </Html>
+    )
+  }
 }
